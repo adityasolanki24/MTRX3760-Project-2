2f556abb5cd37ca5f2a9bd49b02fd35f5551994e
Ayesha Musarrat <131860860+ayeshamusarrat@users.noreply.github.com>
2025-10-21 13:29:28 +1100

    Create battery.cpp

---
+++ battery_monitor/src/battery.cpp
@@ -0,0 +1,8 @@
+// MTRX3760 2025 Project 2: Warehouse Robot DevKit
+// File: battery.cpp
+// Author(s): Ayesha Musarrat
+//
+//  (Intentionally small header-only battery logic for clarity.)
+//  exists to keep the library structure conventional for ament builds
+
+#include "battery_monitor/battery.hpp"
