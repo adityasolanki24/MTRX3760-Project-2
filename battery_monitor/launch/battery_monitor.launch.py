42bf04b89372f0404bfcc0c7800778be1da9eac2
Ayesha Musarrat <131860860+ayeshamusarrat@users.noreply.github.com>
2025-10-28 01:53:30 +1100

    added command-line arguments for custom battery values

--- battery_monitor/launch/battery_monitor.launch.py
+++ battery_monitor/launch/battery_monitor.launch.py
@@ -6,24 +6,32 @@
 # from your project's master launch file.
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    # Declare command-line arguments 
+    #for customisable drain and threshold values at launcg
+    base_drain_arg = DeclareLaunchArgument(
+        "base_drain_per_sec", default_value="1.0",
+        description="Base percentage drain per second")
+    low_threshold_arg = DeclareLaunchArgument(
+        "low_threshold", default_value="20.0",
+        description="Battery percentage threshold for low warning")
+
     return LaunchDescription([
+        base_drain_arg,
+        low_threshold_arg,
+
         Node(
-            package='battery_monitor',
-            executable='battery_monitor_node',
-            name='battery_monitor',
-            output='screen',
+            package="battery_monitor",
+            executable="battery_monitor_node",
+            name="battery_monitor",
             parameters=[{
-                'log_path': '/home/ubuntu/.ros/battery_log.txt',  # change to your VM user if needed
-                'start_percent': 100.0,
-                'base_drain_per_sec': 0.05,
-                'velocity_drain_gain': 0.02,
-                'low_threshold': 20.0,
-                'critical_threshold': 10.0,
-                'use_odom': False,   # set True on robot if you want speed-based drain
-                'tick_ms': 200
-            }]
+                "base_drain_per_sec": LaunchConfiguration("base_drain_per_sec"),
+                "low_threshold": LaunchConfiguration("low_threshold"),
+            }],
+            output="screen"
         )
     ])
