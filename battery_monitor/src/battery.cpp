ec1b20fd6658a4e050beba92a598e26f25f4e02b
Ayesha Musarrat <131860860+ayeshamusarrat@users.noreply.github.com>
2025-10-28 12:29:25 +1100

    Update battery.cpp

--- battery_monitor/src/battery.cpp
+++ battery_monitor/src/battery.cpp
@@ -6,3 +6,6 @@
 //  exists to keep the library structure conventional for ament builds
 
 #include "battery_monitor/battery.hpp"
+
+//for dock_request
+//dock_pub_ = this->create_publisher<std_msgs::msg::Bool>("/dock_request", 10);
